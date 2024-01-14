432ea2aed59aebf7f88c8e0f484cf35f64c768d0
Marleen <marleen@gmail.com>
2024-01-14 11:03:05 -0500

    fix bug

--- GraduationProject/src/app/patientDescriptions/patientDescriptionList.component.ts
+++ GraduationProject/src/app/patientDescriptions/patientDescriptionList.component.ts
@@ -1,27 +1,33 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 
-import { PatinetDescriptionService ,AccountService} from '@app/_services';
+import { PatinetDescriptionService, AccountService } from '@app/_services';
 
 @Component({ templateUrl: 'patientDescriptionList.component.html' })
 export class PatientDescriptionListComponent implements OnInit {
   patinetDescriptions = null;
   currentUser;
-    constructor(private patinetDescriptionService: PatinetDescriptionService,private accountService:AccountService) {}
+  constructor(private patinetDescriptionService: PatinetDescriptionService, private accountService: AccountService) { }
 
-    ngOnInit() {
-      this.accountService.user.subscribe(x => this.currentUser = x);
+  ngOnInit() {
+    this.accountService.user.subscribe(x => {
+      this.currentUser = x
+      this.patinetDescriptionService.getAll()
+        .pipe(first())
+        .subscribe(patinetDescriptions => {
+          debugger
+          if (this.currentUser.userRole == 'Patient') { this.patinetDescriptions = patinetDescriptions.filter(d => d.patientId == this.currentUser.id); }
+          else { this.patinetDescriptions = patinetDescriptions }
+        });
+    });
 
-        this.patinetDescriptionService.getAll()
-            .pipe(first())
-            .subscribe(patinetDescriptions => this.patinetDescriptions = patinetDescriptions);
-    }
+  }
 
-    deletePatientDescription(id: string) {
-        const description = this.patinetDescriptions.find(x => x.id === id);
-        description.isDeleting = true;
-        this.patinetDescriptionService.delete(id)
-            .pipe(first())
-            .subscribe(() => this.patinetDescriptions = this.patinetDescriptions.filter(x => x.id !== id));
-    }
+  deletePatientDescription(id: string) {
+    const description = this.patinetDescriptions.find(x => x.id === id);
+    description.isDeleting = true;
+    this.patinetDescriptionService.delete(id)
+      .pipe(first())
+      .subscribe(() => this.patinetDescriptions = this.patinetDescriptions.filter(x => x.id !== id));
+  }
 }
